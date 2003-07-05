054e330570130ab03da45779d201e9095cc1b738
Robert Kausch <robert.kausch@freac.org>
2003-07-05 20:23:53 +0000

    *** empty log message ***

--- include/smooth/version.h
+++ include/smooth/version.h
@@ -11,13 +11,13 @@
 #ifndef _H_OBJSMOOTH_VERSION_
 #define _H_OBJSMOOTH_VERSION_
 
-#define SMOOTH_VERSION _TEXT("0.8.62.0")
+#define SMOOTH_VERSION _TEXT("0.8.63.0")
 
 #ifdef __SMOOTH_DLL__
-#define SMOOTH_RESVERS 0,8,62,0
+#define SMOOTH_RESVERS 0,8,63,0
 #define SMOOTH_RESPROD 0,8,0,0
 #define SMOOTH_PRODUCT "0.8\0"
-#define SMOOTH_FILEVER "62.0\0"
+#define SMOOTH_FILEVER "63.0\0"
 #endif
 
 #endif

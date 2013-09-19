e03394f0f4d417bb5370e386076a56dda90e9b9a
Robert Kausch <robert.kausch@freac.org>
2013-09-19 22:09:50 +0000

    Added set function to access control class.

--- classes/threads/access.cpp
+++ classes/threads/access.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2012 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2013 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -66,6 +66,17 @@ S::Short S::Threads::Access::Decrement(volatile Short &value)
 	return result;
 }
 
+S::Short S::Threads::Access::Set(volatile Short &value, Short n)
+{
+	mutex->Lock();
+
+	Short	 result = value = n;
+
+	mutex->Release();
+
+	return result;
+}
+
 S::Int S::Threads::Access::Value(volatile Int &value)
 {
 	mutex->Lock();
@@ -93,6 +104,17 @@ S::Int S::Threads::Access::Decrement(volatile Int &value)
 	mutex->Lock();
 
 	Int	 result = --value;
+
+	mutex->Release();
+
+	return result;
+}
+
+S::Int S::Threads::Access::Set(volatile Int &value, Int n)
+{
+	mutex->Lock();
+
+	Int	 result = value = n;
 
 	mutex->Release();
 

eaa703a2d351efb49f24ef71400aa6759d3a77cf
Robert Kausch <robert.kausch@freac.org>
2013-09-28 22:58:10 +0000

    Make sure to register slot only once.

--- tools/translator/section.cpp
+++ tools/translator/section.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2010 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2013 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -94,6 +94,10 @@ Bool Section::Parse(XML::Node *section, List *plist, Array<ListEntry *> &entries
 		}
 	}
 
+	/* OnSelectEntry may already be connected if we parsed a template
+	 * before, so try to disconnect before actually connecting it.
+	 */
+	list->onSelectEntry.Disconnect(&Section::OnSelectEntry, this);
 	list->onSelectEntry.Connect(&Section::OnSelectEntry, this);
 
 	return True;

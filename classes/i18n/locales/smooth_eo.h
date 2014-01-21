c411a591917bd5b59516efa68758f91afa52f99d
Robert Kausch <robert.kausch@freac.org>
2014-01-21 15:34:18 +0000

    Added Esperanto and Valencian language support.

---
+++ classes/i18n/locales/smooth_eo.h
@@ -0,0 +1,81 @@
+ /* The smooth Class Library
+  * Copyright (C) 1998-2014 Robert Kausch <robert.kausch@gmx.net>
+  *
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of "The Artistic License, Version 2.0".
+  *
+  * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE. */
+
+#ifndef H_OBJSMOOTH_I18N_EO
+#define H_OBJSMOOTH_I18N_EO
+
+static const char *smooth_eo = 
+
+	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>					\
+	 <LangFile>									\
+	   <info>									\
+	     <property name=\"program\">smooth</property>				\
+	     <property name=\"language\">Esperanto</property>				\
+	     <property name=\"righttoleft\">false</property>				\
+	     <property name=\"encoding\">UTF-8</property>				\
+	   </info>									\
+	   <data>									\
+	     <entry string=\"OK\">Konfirmu</entry>					\
+	     <entry string=\"Cancel\">Rezignu</entry>					\
+	     <entry string=\"Yes\">Jes</entry>						\
+	     <entry string=\"No\">Ne</entry>						\
+	     <entry string=\"Retry\">Reprovu</entry>					\
+	     <entry string=\"Abort\">Haltu</entry>					\
+	     <entry string=\"Ignore\">Ignoru</entry>					\
+											\
+	     <entry string=\"Undo\">Malfari</entry>					\
+	     <entry string=\"Cut\">Eltondi</entry>					\
+	     <entry string=\"Copy\">Kopii</entry>					\
+	     <entry string=\"Paste\">Enmeti</entry>					\
+	     <entry string=\"Clear\">Forviŝi</entry>					\
+	     <entry string=\"Select all\">Elekti ĉion</entry>				\
+											\
+	     <entry string=\"Backspace\">Retro</entry>					\
+	     <entry string=\"Tab\">Tab</entry>						\
+	     <entry string=\"Return\">Konfirm</entry>					\
+	     <entry string=\"Esc\">Eskap</entry>					\
+	     <entry string=\"Space\">Spac</entry>					\
+	     <entry string=\"PgUp\">Paĝo supren</entry>					\
+	     <entry string=\"PgDown\">Paĝo malsupren</entry>				\
+	     <entry string=\"End\">Fino</entry>						\
+	     <entry string=\"Home\">Hejm</entry>					\
+	     <entry string=\"Left\">Maldekstren</entry>					\
+	     <entry string=\"Up\">Supren</entry>					\
+	     <entry string=\"Right\">Dekstren</entry>					\
+	     <entry string=\"Down\">Malsupren</entry>					\
+	     <entry string=\"Ins\">Enmet</entry>					\
+	     <entry string=\"Del\">For</entry>						\
+	     <entry string=\"Ctrl\">Ktrl</entry>					\
+	     <entry string=\"Alt\">Alt</entry>						\
+	     <entry string=\"Shift\">Majuskl</entry>					\
+											\
+	     <entry string=\"Open file\">Malfermi dosieron</entry>			\
+	     <entry string=\"Save file\">Konservi dosieron</entry>			\
+	     <entry string=\"Save file as\">Konservi dosieron kiel</entry>		\
+	     <entry string=\"Select directory\">Elekti dosierujon</entry>		\
+	     <entry string=\"Select font\">Elekti tiparon</entry>			\
+											\
+	     <entry string=\"Color selection\">Elekti koloron</entry>			\
+	     <entry string=\"HTML code\">HTML-kodo</entry>				\
+	     <entry string=\"R\">R</entry>						\
+	     <entry string=\"G\">G</entry>						\
+	     <entry string=\"B\">B</entry>						\
+	     <entry string=\"H\">H</entry>						\
+	     <entry string=\"S\">S</entry>						\
+	     <entry string=\"V\">V</entry>						\
+											\
+	     <entry string=\"Tip of the day\">Konsileto de la tago</entry>		\
+	     <entry string=\"Did you know...\">Ĉu vi sciis...</entry>			\
+	     <entry string=\"Show tips on startup\">Montri konsiletojn ĉe lanĉo</entry>	\
+	     <entry string=\"Next tip\">Sekva konsileto</entry>				\
+	   </data>									\
+	 </LangFile>";
+
+#endif
---
+++ classes/i18n/locales/smooth_va.h
@@ -0,0 +1,81 @@
+ /* The smooth Class Library
+  * Copyright (C) 1998-2014 Robert Kausch <robert.kausch@gmx.net>
+  *
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of "The Artistic License, Version 2.0".
+  *
+  * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE. */
+
+#ifndef H_OBJSMOOTH_I18N_VA
+#define H_OBJSMOOTH_I18N_VA
+
+static const char *smooth_va = 
+
+	"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>				\
+	 <LangFile>									\
+	   <info>									\
+	     <property name=\"program\">smooth</property>				\
+	     <property name=\"language\">Valencian / Valencià</property>		\
+	     <property name=\"righttoleft\">false</property>				\
+	     <property name=\"encoding\">ISO-8859-1</property>				\
+	   </info>									\
+	   <data>									\
+	     <entry string=\"OK\">Bé</entry>						\
+	     <entry string=\"Cancel\">Cancel·la</entry>					\
+	     <entry string=\"Yes\">Sí</entry>						\
+	     <entry string=\"No\">No</entry>						\
+	     <entry string=\"Retry\">Reintenta</entry>					\
+	     <entry string=\"Abort\">Interromp</entry>					\
+	     <entry string=\"Ignore\">Ignora</entry>					\
+											\
+	     <entry string=\"Undo\">Desfés</entry>					\
+	     <entry string=\"Cut\">Talla</entry>					\
+	     <entry string=\"Copy\">Copia</entry>					\
+	     <entry string=\"Paste\">Apega</entry>					\
+	     <entry string=\"Clear\">Neteja</entry>					\
+	     <entry string=\"Select all\">Selecciona-ho tot</entry>			\
+											\
+	     <entry string=\"Backspace\">Retrocés</entry>				\
+	     <entry string=\"Tab\">Tab</entry>						\
+	     <entry string=\"Return\">Retorn</entry>					\
+	     <entry string=\"Esc\">Esc</entry>						\
+	     <entry string=\"Space\">Espai</entry>					\
+	     <entry string=\"PgUp\">RePàg</entry>					\
+	     <entry string=\"PgDown\">AvPàg</entry>					\
+	     <entry string=\"End\">Fi</entry>						\
+	     <entry string=\"Home\">Inici</entry>					\
+	     <entry string=\"Left\">Esquerra</entry>					\
+	     <entry string=\"Up\">Amunt</entry>						\
+	     <entry string=\"Right\">Dreta</entry>					\
+	     <entry string=\"Down\">Avall</entry>					\
+	     <entry string=\"Ins\">Ins</entry>						\
+	     <entry string=\"Del\">Supr</entry>						\
+	     <entry string=\"Ctrl\">Ctrl</entry>					\
+	     <entry string=\"Alt\">Alt</entry>						\
+	     <entry string=\"Shift\">Maj</entry>					\
+											\
+	     <entry string=\"Open file\">Obri fitxer</entry>				\
+	     <entry string=\"Save file\">Guarda el fitxer</entry>			\
+	     <entry string=\"Save file as\">Guarda el fitxer com a</entry>		\
+	     <entry string=\"Select directory\">Selecció de carpeta</entry>		\
+	     <entry string=\"Select font\">Trieu el tipus de lletra</entry>		\
+											\
+	     <entry string=\"Color selection\">Selecció de color</entry>		\
+	     <entry string=\"HTML code\">Codi HTML</entry>				\
+	     <entry string=\"R\">R</entry>						\
+	     <entry string=\"G\">G</entry>						\
+	     <entry string=\"B\">B</entry>						\
+	     <entry string=\"H\">H</entry>						\
+	     <entry string=\"S\">S</entry>						\
+	     <entry string=\"V\">V</entry>						\
+											\
+	     <entry string=\"Tip of the day\">Pista del dia</entry>			\
+	     <entry string=\"Did you know...\">Sabíeu que...</entry>			\
+	     <entry string=\"Show tips on startup\">Mostra pistes en engegar</entry>	\
+	     <entry string=\"Next tip\">Següent</entry>					\
+	   </data>									\
+	 </LangFile>";
+
+#endif

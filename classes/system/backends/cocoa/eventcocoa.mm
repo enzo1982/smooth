5035d9802887ee03b41d150b25b3dcd0014e27d4
Robert Kausch <robert.kausch@freac.org>
2014-01-31 16:02:19 +0000

    Fix for Mavericks compatibility.

--- classes/system/backends/cocoa/eventcocoa.mm
+++ classes/system/backends/cocoa/eventcocoa.mm
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2013 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2014 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -49,6 +49,20 @@ S::Int S::System::EventCocoa::ProcessNextEvent()
 
 				break;
 			}
+		}
+
+		/* The first loop invocation must not be modal
+		 * to prevent problems on OS X 10.9 Mavericks.
+		 */
+		static Bool	 ranNonModal = False;
+
+		if (!ranNonModal)
+		{
+			ranNonModal = True;
+
+			[NSApp run];
+
+			return Success();
 		}
 
 		/* Run modal or regular loop.

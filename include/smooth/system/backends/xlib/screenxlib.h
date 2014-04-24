c3a19c8e4941afb262c324c9b433c93979e89ddb
Robert Kausch <robert.kausch@freac.org>
2014-04-24 17:59:35 +0000

    Removed superfluous includes.

--- include/smooth/system/backends/xlib/screenxlib.h
+++ include/smooth/system/backends/xlib/screenxlib.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2013 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2014 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -31,7 +31,6 @@ namespace X11
 #		define Success	0
 
 #		include <X11/Xlib.h>
-#		include <X11/Xmu/Atoms.h>
 
 #		undef True
 #		undef False

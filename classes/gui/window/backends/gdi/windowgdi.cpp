96ba7c5c9204fa5b4be12d7edaba4d97cd8e488b
Robert Kausch <robert.kausch@freac.org>
2016-01-31 16:15:03 +0000

    Do not send position updates for minimized windows.

--- classes/gui/window/backends/gdi/windowgdi.cpp
+++ classes/gui/window/backends/gdi/windowgdi.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2015 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2016 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -494,6 +494,7 @@ S::Int S::GUI::WindowGDI::ProcessSystemMessages(UINT message, WPARAM wParam, LPA
 		/* Window state change messages:
 		 */
 		case WM_WINDOWPOSCHANGED:
+			if (!IsIconic(hwnd))
 			{
 				WINDOWPOS	*windowPos = (LPWINDOWPOS) lParam;
 

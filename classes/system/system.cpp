dcad6458542453db564e8153145220a351d463d3
Robert Kausch <robert.kausch@freac.org>
2017-01-14 13:27:49 +0000

    Pass SW_SHOW when opening URLs on Windows.

--- classes/system/system.cpp
+++ classes/system/system.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2016 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2017 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -208,7 +208,7 @@ S::Void S::System::System::Shutdown()
 S::Bool S::System::System::OpenURL(const String &url)
 {
 #if defined __WIN32__
-	ShellExecute(NULL, (wchar_t *) L"open", url, NULL, NULL, 0);
+	ShellExecute(NULL, (wchar_t *) L"open", url, NULL, NULL, SW_SHOW);
 #elif defined __APPLE__
 	const char	*args = url;
 

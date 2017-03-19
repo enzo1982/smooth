dab08893055354ee040490c87755abe669bbef27
Robert Kausch <robert.kausch@freac.org>
2017-03-19 15:33:27 +0000

    Fix crash at program exit on OpenBSD.

--- classes/system/dynamicloader.cpp
+++ classes/system/dynamicloader.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2016 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2017 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -112,9 +112,9 @@ S::System::DynamicLoader::~DynamicLoader()
 #ifdef __WIN32__
 	if (handle != NIL) FreeLibrary((HINSTANCE) handle);
 #else
-#ifdef __HAIKU__
-	/* Haiku does not support RTLD_NODELETE yet, so we
-	 * cannot close .so files without risking a crash.
+#if defined __OpenBSD__ || defined __HAIKU__
+	/* OpenBSD and Haiku do not support RTLD_NODELETE yet,
+	 * so we cannot close .so files without risking a crash.
 	 */
 #else
 	if (handle != NIL) dlclose(handle);

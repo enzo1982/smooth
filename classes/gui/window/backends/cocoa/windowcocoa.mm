bacb6da674e70676e7ef08d37cc25a16c4d6941e
Robert Kausch <robert.kausch@freac.org>
2017-10-23 20:57:07 +0200

    Fixed macOS 10.13 compatibility issue.

--- classes/gui/window/backends/cocoa/windowcocoa.mm
+++ classes/gui/window/backends/cocoa/windowcocoa.mm
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2016 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2017 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -1344,7 +1344,8 @@ S::Int S::GUI::WindowCocoa::SetProgressIndicator(Window::ProgressIndicatorState 
 			break;
 	}
 
-	[[NSApp dockTile] display];
+	if ([NSThread isMainThread]) [[NSApp dockTile] display];
+	else			     [[NSApp dockTile] performSelectorOnMainThread: @selector(display) withObject: nil waitUntilDone: NO];
 
 	return Success();
 }

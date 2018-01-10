d9616476cf6a021d1b7646ee8b6c96b02b3af09d
Robert Kausch <robert.kausch@freac.org>
2018-01-10 21:29:45 +0100

    Remove a hack that should be obsolete with the latest changes to the Widget/Layer classes.

--- classes/gui/widgets/special/cursor.cpp
+++ classes/gui/widgets/special/cursor.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2017 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2018 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -724,13 +724,6 @@ S::Int S::GUI::Cursor::Scroll(Int nScrollPos)
 
 S::Void S::GUI::Cursor::OnTimer()
 {
-	if (!IsVisible())
-	{
-		onLoseFocus.Emit();
-
-		return;
-	}
-
 	ShowCursor(!promptVisible);
 }
 

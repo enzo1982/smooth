6db2979fb1a2b91482b77e7c4e5a481218c3b31a
Robert Kausch <robert.kausch@freac.org>
2019-01-12 11:57:45 +0100

    Evaluate TMPDIR environment variable to find temp directory.

--- classes/system/system.cpp
+++ classes/system/system.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2018 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2019 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -588,7 +588,9 @@ S::String S::System::System::GetTempDirectory()
 
 	tempDir = buffer;
 #else
-	tempDir = "/var/tmp";
+	tempDir = getenv("TMPDIR");
+
+	if (tempDir == NIL) tempDir = "/var/tmp";
 #endif
 
 	if (!tempDir.EndsWith(Directory::GetDirectoryDelimiter())) tempDir.Append(Directory::GetDirectoryDelimiter());

7394132e7c06e163856a83195a1d0d99eb1b5357
Robert Kausch <robert.kausch@freac.org>
2019-07-09 20:20:01 +0200

    Use GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER action to work around quirky behavior of GTK_FILE_CHOOSER_ACTION_CREATE_FOLDER.

--- classes/gui/dialogs/directory/dirdlg_gtk.cpp
+++ classes/gui/dialogs/directory/dirdlg_gtk.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2018 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2019 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -21,7 +21,7 @@ const Error &S::GUI::Dialogs::DirSelection::ShowDialog()
 
 	/* Create file chooser dialog.
 	 */
-	GtkWidget	*dialog = gtk_file_chooser_dialog_new(caption, NULL, GTK_FILE_CHOOSER_ACTION_CREATE_FOLDER,
+	GtkWidget	*dialog = gtk_file_chooser_dialog_new(caption, NULL, GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
 							      GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
 							      GTK_STOCK_OK, GTK_RESPONSE_ACCEPT,
 							      NULL);

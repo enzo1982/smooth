9d559823d92a12485deec1365fd7c421ce123572
Robert Kausch <robert.kausch@freac.org>
2019-10-22 22:26:29 +0200

    Reset action handler for menu entries with a submenu on macOS.

--- classes/gui/widgets/multi/menu/menubar_cocoa.mm
+++ classes/gui/widgets/multi/menu/menubar_cocoa.mm
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2018 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2019 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -107,6 +107,7 @@ class CocoaMenuEntryDelegate
 							   ignoreLastEntry: ignoreLastEntry];
 
 			[self setSubmenu: submenu];
+			[self setAction: nil];
 		}
 
 		if (menuEntry->GetShortcut() != NIL)

a2fda220e68d44c49e15552c07c41c63f13970f3
Robert Kausch <robert.kausch@freac.org>
2020-01-05 17:39:52 +0100

    Fix possible crash when exiting programs on Haiku.

--- classes/gui/window/backends/haiku/windowhaiku.cpp
+++ classes/gui/window/backends/haiku/windowhaiku.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2019 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2020 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -573,10 +573,11 @@ S::Int S::GUI::WindowHaiku::Close()
 	 */
 	BWindow	*oldwnd = wnd;
 
+	oldwnd->Lock();
+
 	wnd  = NIL;
 	view = NIL;
 
-	oldwnd->Lock();
 	oldwnd->Quit();
 
 	/* Resume the application lock.

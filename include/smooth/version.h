cf86190d1ebc883abfa4f21c20f8c932b5101c9b
Robert Kausch <robert.kausch@freac.org>
2020-01-12 17:54:30 +0100

    smooth release 0.9.1.

--- include/smooth/version.h
+++ include/smooth/version.h
@@ -15,10 +15,10 @@
 #define SMOOTH_APIVERSION "0.9.0"
 
 #ifdef SMOOTH_DLL
-# define SMOOTH_RESVERS	   0,9,1,5149
-# define SMOOTH_RESPROD	   0,9,1,5149
+# define SMOOTH_RESVERS	   0,9,1,5184
+# define SMOOTH_RESPROD	   0,9,1,5184
 # define SMOOTH_PRODUCT	  "0.9\0"
-# define SMOOTH_FILEVER	  "0.9.1.5149\0"
+# define SMOOTH_FILEVER	  "0.9.1.5184\0"
 #endif
 
 #endif

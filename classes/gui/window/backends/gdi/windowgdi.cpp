076f0ed86774ff78c05ae348abf1bda466e880ec
Robert Kausch <robert.kausch@freac.org>
2020-02-09 16:25:02 +0100

    Fix mouse events still arriving at parent window after opening popup menu.

--- classes/gui/window/backends/gdi/windowgdi.cpp
+++ classes/gui/window/backends/gdi/windowgdi.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2019 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2020 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -552,6 +552,10 @@ S::Int S::GUI::WindowGDI::ProcessSystemMessages(UINT message, WPARAM wParam, LPA
 
 		case WM_KILLFOCUS:
 			Input::Keyboard::ResetKeyState();
+
+			/* Release mouse capture.
+			 */
+			ReleaseCapture();
 
 			/* Get the window that now has the focus.
 			 */

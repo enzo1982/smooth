c6ff4bccc1a93aad47f89ce5bf17d1373bf109b7
XilizaX <saqazadeh@cyberservices.com>
2020-07-18 15:41:43 +0200

    Update Persian translation.

--- classes/i18n/locales/smooth_fa.h
+++ classes/i18n/locales/smooth_fa.h
@@ -27,43 +27,24 @@ static const char *smooth_fa =
 	     <entry string=\"Yes\">بله</entry>						\
 	     <entry string=\"No\">نه</entry>						\
 	     <entry string=\"Retry\">سعی مجدد</entry>					\
-	     <entry string=\"Abort\">ساقط کردن</entry>					\
-	     <entry string=\"Ignore\">چشم‌پوشی</entry>					\
+	     <entry string=\"Abort\">لغو</entry>					\
+	     <entry string=\"Ignore\">چشم پوشي</entry>					\
 											\
 	     <entry string=\"Undo\">واگرد</entry>					\
 	     <entry string=\"Cut\">برش</entry>						\
-	     <entry string=\"Copy\">رونوشت</entry>					\
+	     <entry string=\"Copy\">کپي</entry>						\
 	     <entry string=\"Paste\">چسباندن</entry>					\
 	     <entry string=\"Clear\">پاک کردن</entry>					\
 	     <entry string=\"Select all\">برگزیدن همه</entry>				\
 											\
-	     <entry string=\"Backspace\">پس‌بر</entry>					\
-	     <entry string=\"Tab\">جهش</entry>						\
-	     <entry string=\"Return\">بازگشت</entry>					\
-	     <entry string=\"Esc\">گریز</entry>						\
-	     <entry string=\"Space\">فاصله</entry>					\
-	     <entry string=\"PgUp\">بالابر صفحه</entry>					\
-	     <entry string=\"PgDown\">پایین‌بر صفحه</entry>				\
-	     <entry string=\"End\">پایان</entry>					\
-	     <entry string=\"Home\">آغازه</entry>					\
-	     <entry string=\"Left\">چپ</entry>						\
-	     <entry string=\"Up\">بال</entry>						\
-	     <entry string=\"Right\">راست</entry>					\
-	     <entry string=\"Down\">پایین</entry>					\
-	     <entry string=\"Ins\">درج</entry>						\
-	     <entry string=\"Del\">حذف</entry>						\
-	     <entry string=\"Ctrl\">مهار</entry>					\
-	     <entry string=\"Alt\">دگرساز</entry>					\
-	     <entry string=\"Shift\">تبدیل</entry>					\
-											\
-	     <entry string=\"Open file\">باز کردن پرونده</entry>			\
-	     <entry string=\"Save file\">ذخیرۀ پرونده</entry>				\
-	     <entry string=\"Save file as\">ذخیرۀ پرونده به عنوان</entry>		\
+	     <entry string=\"Open file\">باز کردن فايل</entry>				\
+	     <entry string=\"Save file\">ذخیرۀ فايل</entry>				\
+	     <entry string=\"Save file as\">ذخیرۀ فايل به عنوان</entry>			\
 	     <entry string=\"Select directory\">برگزیدن پوشه</entry>			\
-	     <entry string=\"Select font\">برگزیدن قلم</entry>				\
+	     <entry string=\"Select font\">برگزیدن فونت</entry>				\
 											\
 	     <entry string=\"Color selection\">برگزیدن رنگ</entry>			\
-	     <entry string=\"HTML code\">کد اچ</entry>					\
+	     <entry string=\"HTML code\">HTML کد</entry>				\
 											\
 	     <entry string=\"Tip of the day\">نکتۀ روز</entry>				\
 	     <entry string=\"Did you know...\">آیا می‌دانید...</entry>			\

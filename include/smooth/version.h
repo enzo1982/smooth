46ff6ba4ec96bf76d6efb01e4b1b1122387cfa14
Robert Kausch <robert.kausch@freac.org>
2021-02-14 14:59:05 +0100

    smooth release 0.9.7.

--- include/smooth/version.h
+++ include/smooth/version.h
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2020 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2021 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -15,10 +15,10 @@
 #define SMOOTH_APIVERSION "0.9.0"
 
 #ifdef SMOOTH_DLL
-# define SMOOTH_RESVERS	   0,9,7,5505
-# define SMOOTH_RESPROD	   0,9,7,5505
+# define SMOOTH_RESVERS	   0,9,7,5523
+# define SMOOTH_RESPROD	   0,9,7,5523
 # define SMOOTH_PRODUCT	  "0.9\0"
-# define SMOOTH_FILEVER	  "0.9.7.5505\0"
+# define SMOOTH_FILEVER	  "0.9.7.5523\0"
 #endif
 
 #endif

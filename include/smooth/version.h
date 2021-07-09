df3635d97ed41dfb1c983f4fa3fa7e14a2651e17
Robert Kausch <robert.kausch@freac.org>
2021-07-09 21:51:29 +0200

    Update libcpuid to version 0.5.1.

--- include/smooth/version.h
+++ include/smooth/version.h
@@ -11,14 +11,14 @@
 #ifndef H_OBJSMOOTH_VERSION
 #define H_OBJSMOOTH_VERSION
 
-#define SMOOTH_VERSION	  "0.9.8"
+#define SMOOTH_VERSION	  "0.9.9"
 #define SMOOTH_APIVERSION "0.9.0"
 
 #ifdef SMOOTH_DLL
-# define SMOOTH_RESVERS	   0,9,8,5594
-# define SMOOTH_RESPROD	   0,9,8,5594
+# define SMOOTH_RESVERS	   0,9,9,5595
+# define SMOOTH_RESPROD	   0,9,9,5595
 # define SMOOTH_PRODUCT	  "0.9\0"
-# define SMOOTH_FILEVER	  "0.9.8.5594\0"
+# define SMOOTH_FILEVER	  "0.9.9.5595\0"
 #endif
 
 #endif

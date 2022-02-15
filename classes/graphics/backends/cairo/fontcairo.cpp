1e30f16f7836d5684f5142ec85080a3c12c7e701
Robert Kausch <robert.kausch@freac.org>
2022-02-15 21:28:52 +0100

    Fix compilation on FreeBSD.

--- classes/graphics/backends/cairo/fontcairo.cpp
+++ classes/graphics/backends/cairo/fontcairo.cpp
@@ -1,5 +1,5 @@
  /* The smooth Class Library
-  * Copyright (C) 1998-2021 Robert Kausch <robert.kausch@gmx.net>
+  * Copyright (C) 1998-2022 Robert Kausch <robert.kausch@gmx.net>
   *
   * This library is free software; you can redistribute it and/or
   * modify it under the terms of "The Artistic License, Version 2.0".
@@ -7,6 +7,8 @@
   * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
   * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
   * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE. */
+
+#include <type_traits>
 
 #include <smooth/graphics/backends/cairo/fontcairo.h>
 #include <smooth/graphics/surface.h>

70024ac6319dac165fe58524b5758a69d6d63e88
Robert Kausch <robert.kausch@freac.org>
2023-01-17 00:33:24 +0100

    Update libxml2 to version 2.10.3.

--- include/support/libxml2/config.h
+++ include/support/libxml2/config.h
@@ -1,13 +1,7 @@
-/* This file is needed to compile our custom libxml2 on Linux and FreeBSD.
- */
-
+#if defined(WIN32) && !defined(__CYGWIN__)
+#include "win32config.h"
+#else
 #define HAVE_STDINT_H 1
-#define HAVE_INTTYPES_H 1
-#define HAVE_CTYPE_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_ERRNO_H 1
-#define HAVE_ICONV_H 1
-
-#define ICONV_CONST const
 
 #define HAVE_VA_COPY 1
+#endif

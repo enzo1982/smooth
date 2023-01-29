485644f1004b980d9e4fa3a6532309e3975bd2d5
Patriccollu <patriccollu@gmail.com>
2023-01-30 00:56:54 +0100

    Update Corsican translation (#7)

--- classes/i18n/locales/smooth_co.h
+++ classes/i18n/locales/smooth_co.h
@@ -24,11 +24,37 @@ static const char *smooth_co =
 	   <data>										\
 	     <entry string=\"OK\">Vai</entry>							\
 	     <entry string=\"Cancel\">Abbandunà</entry>						\
-	     <entry string=\"Yes\">Iè</entry>							\
-	     <entry string=\"No\">Innò</entry>							\
+	     <entry string=\"Yes\">Sì</entry>							\
+	     <entry string=\"No\">Nò</entry>							\
 	     <entry string=\"Retry\">Torna</entry>						\
 	     <entry string=\"Abort\">Interrompe</entry>						\
 	     <entry string=\"Ignore\">Ignurà</entry>						\
+												\
+	     <entry string=\"Undo\">Disfà</entry>						\
+	     <entry string=\"Cut\">Taglià</entry>						\
+	     <entry string=\"Copy\">Cupià</entry>						\
+	     <entry string=\"Paste\">Incullà</entry>						\
+	     <entry string=\"Clear\">Viutà</entry>						\
+	     <entry string=\"Select all\">Tuttu selezziunà</entry>				\
+												\
+	     <entry string=\"Backspace\">Currezzione</entry>					\
+	     <entry string=\"Tab\">Tab</entry>							\
+	     <entry string=\"Return\">Entrata</entry>						\
+	     <entry string=\"Esc\">Scap</entry>							\
+	     <entry string=\"Space\">Spaziu</entry>						\
+	     <entry string=\"PgUp\">Pag. sù</entry>						\
+	     <entry string=\"PgDown\">Pag. ghjò</entry>						\
+	     <entry string=\"End\">Fine</entry>							\
+	     <entry string=\"Home\">Accolta</entry>						\
+	     <entry string=\"Left\">Manca</entry>						\
+	     <entry string=\"Up\">Insù</entry>							\
+	     <entry string=\"Right\">Diritta</entry>						\
+	     <entry string=\"Down\">Inghjò</entry>						\
+	     <entry string=\"Ins\">Fram</entry>							\
+	     <entry string=\"Del\">Squas</entry>						\
+	     <entry string=\"Ctrl\">Ctrl</entry>						\
+	     <entry string=\"Alt\">Alt</entry>							\
+	     <entry string=\"Shift\">Maius</entry>						\
 												\
 	     <entry string=\"Open file\">Apre u schedariu</entry>				\
 	     <entry string=\"Save file\">Arregistrà u schedariu</entry>				\
@@ -45,10 +71,10 @@ static const char *smooth_co =
 	     <entry string=\"S\">S</entry>							\
 	     <entry string=\"V\">V</entry>							\
 												\
-	     <entry string=\"Tip of the day\">Minichichja oghjinca</entry>			\
+	     <entry string=\"Tip of the day\">Astuzia oghjinca</entry>				\
 	     <entry string=\"Did you know...\">A sapete chì…</entry>				\
-	     <entry string=\"Show tips on startup\">Affissà e minichichje à l’avvià</entry>	\
-	     <entry string=\"Next tip\">Minichichja seguente</entry>				\
+	     <entry string=\"Show tips on startup\">Affissà l’astuzie à u lanciu</entry>	\
+	     <entry string=\"Next tip\">Astuzia seguente</entry>				\
 	   </data>										\
 	 </LangFile>";
 

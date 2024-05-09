9a6a0edc2e8d14a0b533f7bc1cb92023ffeaf244
Robert Kausch <robert.kausch@freac.org>
2024-05-09 18:28:42 +0200

    Update fribidi to version 1.0.14.

--- libraries/fribidi/fribidi-config.h
+++ libraries/fribidi/fribidi-config.h
@@ -4,13 +4,13 @@
 #define FRIBIDI_CONFIG_H
 
 #define FRIBIDI "fribidi"
-#define FRIBIDI_NAME "fribidi"
-#define FRIBIDI_BUGREPORT "fribidi"
+#define FRIBIDI_NAME "GNU FriBidi"
+#define FRIBIDI_BUGREPORT "https://github.com/fribidi/fribidi/issues/new"
 
-#define FRIBIDI_VERSION "1.0.13"
+#define FRIBIDI_VERSION "1.0.14"
 #define FRIBIDI_MAJOR_VERSION 1
 #define FRIBIDI_MINOR_VERSION 0
-#define FRIBIDI_MICRO_VERSION 13
+#define FRIBIDI_MICRO_VERSION 14
 #define FRIBIDI_INTERFACE_VERSION 4
 #define FRIBIDI_INTERFACE_VERSION_STRING "4"
 
